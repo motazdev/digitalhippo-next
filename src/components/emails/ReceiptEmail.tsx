f431bdb4814734e28be13a326c3b62f9581d3fa1
motaz essam <motazessamdev@gmail.com>
2024-09-13 20:41:26 +0300

    edit commit

--- src/components/emails/ReceiptEmail.tsx
+++ src/components/emails/ReceiptEmail.tsx
@@ -1,5 +1,4 @@
-import { formatPrice } from "../../lib/utils";
-import { Product } from "../../payload-types";
+import { formatPrice } from '../../lib/utils'
 
 import {
   Body,
@@ -15,26 +14,22 @@ import {
   Section,
   Text,
   render,
-} from "@react-email/components";
+} from '@react-email/components'
 
-import * as React from "react";
+import * as React from 'react'
 
-import { format } from "date-fns";
+import { format } from 'date-fns'
+import { Product } from 'payload-types'
 
 interface ReceiptEmailProps {
-  email: string;
-  date: Date;
-  orderId: string;
-  products: Product[];
+  email: string
+  date: Date
+  orderId: string
+  products: Product[]
 }
 
-export const ReceiptEmail = ({
-  email,
-  date,
-  orderId,
-  products,
-}: ReceiptEmailProps) => {
-  const total = products.reduce((acc, curr) => acc + curr.price, 0) + 1;
+export const ReceiptEmail = ({ email, date, orderId, products }: ReceiptEmailProps) => {
+  const total = products.reduce((acc, curr) => acc + curr.price, 0) + 1
 
   return (
     <Html>
@@ -72,9 +67,7 @@ export const ReceiptEmail = ({
 
               <Column style={informationTableColumn}>
                 <Text style={informationTableLabel}>INVOICE DATE</Text>
-                <Text style={informationTableValue}>
-                  {format(date, "dd MMM yyyy")}
-                </Text>
+                <Text style={informationTableValue}>{format(date, 'dd MMM yyyy')}</Text>
               </Column>
 
               <Column style={informationTableColumn}>
@@ -93,12 +86,12 @@ export const ReceiptEmail = ({
             <Text style={productsTitle}>Order Summary</Text>
           </Section>
           {products.map((product) => {
-            const { image } = product.images[0];
+            const { image } = product.images[0]
 
             return (
               <Section key={product.id}>
-                <Column style={{ width: "64px" }}>
-                  {typeof image !== "string" && image.url ? (
+                <Column style={{ width: '64px' }}>
+                  {typeof image !== 'string' && image.url ? (
                     <Img
                       src={image.url}
                       width="64"
@@ -108,12 +101,12 @@ export const ReceiptEmail = ({
                     />
                   ) : null}
                 </Column>
-                <Column style={{ paddingLeft: "22px" }}>
+                <Column style={{ paddingLeft: '22px' }}>
                   <Text style={productTitle}>{product.name}</Text>
                   {product.description ? (
                     <Text style={productDescription}>
                       {product.description.length > 50
-                        ? product.description?.slice(0, 50) + "..."
+                        ? product.description?.slice(0, 50) + '...'
                         : product.description}
                     </Text>
                   ) : null}
@@ -129,14 +122,14 @@ export const ReceiptEmail = ({
                   <Text style={productPrice}>{formatPrice(product.price)}</Text>
                 </Column>
               </Section>
-            );
+            )
           })}
 
           <Section>
-            <Column style={{ width: "64px" }}></Column>
+            <Column style={{ width: '64px' }}></Column>
             <Column
               style={{
-                paddingLeft: "40px",
+                paddingLeft: '40px',
                 paddingTop: 20,
               }}
             >
@@ -161,178 +154,176 @@ export const ReceiptEmail = ({
           <Hr style={productPriceLineBottom} />
 
           <Text style={footerLinksWrapper}>
-            <Link href="#">Account Settings</Link> •{" "}
-            <Link href="#">Terms of Sale</Link> •{" "}
+            <Link href="#">Account Settings</Link> • <Link href="#">Terms of Sale</Link> •{' '}
             <Link href="#">Privacy Policy </Link>
           </Text>
           <Text style={footerCopyright}>
-            Copyright © 2023 DigitalHippo Inc. <br />{" "}
-            <Link href="#">All rights reserved</Link>
+            Copyright © 2023 DigitalHippo Inc. <br /> <Link href="#">All rights reserved</Link>
           </Text>
         </Container>
       </Body>
     </Html>
-  );
-};
+  )
+}
 
 export const ReceiptEmailHtml = (props: ReceiptEmailProps) =>
   render(<ReceiptEmail {...props} />, {
     pretty: true,
-  });
+  })
 
 const main = {
   fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
-  backgroundColor: "#ffffff",
-};
+  backgroundColor: '#ffffff',
+}
 
 const resetText = {
-  margin: "0",
-  padding: "0",
+  margin: '0',
+  padding: '0',
   lineHeight: 1.4,
-};
+}
 
 const container = {
-  margin: "0 auto",
-  padding: "20px 0 48px",
-  width: "660px",
-};
+  margin: '0 auto',
+  padding: '20px 0 48px',
+  width: '660px',
+}
 
-const tableCell = { display: "table-cell" };
+const tableCell = { display: 'table-cell' }
 
 const heading = {
-  fontSize: "28px",
-  fontWeight: "300",
-  color: "#888888",
-};
+  fontSize: '28px',
+  fontWeight: '300',
+  color: '#888888',
+}
 
 const informationTable = {
-  borderCollapse: "collapse" as const,
-  borderSpacing: "0px",
-  color: "rgb(51,51,51)",
-  backgroundColor: "rgb(250,250,250)",
-  borderRadius: "3px",
-  fontSize: "12px",
-  marginTop: "12px",
-};
+  borderCollapse: 'collapse' as const,
+  borderSpacing: '0px',
+  color: 'rgb(51,51,51)',
+  backgroundColor: 'rgb(250,250,250)',
+  borderRadius: '3px',
+  fontSize: '12px',
+  marginTop: '12px',
+}
 
 const informationTableRow = {
-  height: "46px",
-};
+  height: '46px',
+}
 
 const informationTableColumn = {
-  paddingLeft: "20px",
-  borderStyle: "solid",
-  borderColor: "white",
-  borderWidth: "0px 1px 1px 0px",
-  height: "44px",
-};
+  paddingLeft: '20px',
+  borderStyle: 'solid',
+  borderColor: 'white',
+  borderWidth: '0px 1px 1px 0px',
+  height: '44px',
+}
 
 const informationTableLabel = {
   ...resetText,
-  color: "rgb(102,102,102)",
-  fontSize: "10px",
-};
+  color: 'rgb(102,102,102)',
+  fontSize: '10px',
+}
 
 const informationTableValue = {
-  fontSize: "12px",
-  margin: "0",
-  padding: "0",
+  fontSize: '12px',
+  margin: '0',
+  padding: '0',
   lineHeight: 1.4,
-};
+}
 
 const productTitleTable = {
   ...informationTable,
-  margin: "30px 0 15px 0",
-  height: "24px",
-};
+  margin: '30px 0 15px 0',
+  height: '24px',
+}
 
 const productsTitle = {
-  background: "#fafafa",
-  paddingLeft: "10px",
-  fontSize: "14px",
-  fontWeight: "500",
-  margin: "0",
-};
+  background: '#fafafa',
+  paddingLeft: '10px',
+  fontSize: '14px',
+  fontWeight: '500',
+  margin: '0',
+}
 
 const productIcon = {
-  margin: "0 0 0 20px",
-  borderRadius: "14px",
-  border: "1px solid rgba(128,128,128,0.2)",
-};
+  margin: '0 0 0 20px',
+  borderRadius: '14px',
+  border: '1px solid rgba(128,128,128,0.2)',
+}
 
 const productTitle = {
-  fontSize: "12px",
-  fontWeight: "600",
+  fontSize: '12px',
+  fontWeight: '600',
   ...resetText,
-};
+}
 
 const productDescription = {
-  fontSize: "12px",
-  color: "rgb(102,102,102)",
+  fontSize: '12px',
+  color: 'rgb(102,102,102)',
   ...resetText,
-};
+}
 
 const productLink = {
-  fontSize: "12px",
-  color: "rgb(0,112,201)",
-  textDecoration: "none",
-};
+  fontSize: '12px',
+  color: 'rgb(0,112,201)',
+  textDecoration: 'none',
+}
 
 const productPriceTotal = {
-  margin: "0",
-  color: "rgb(102,102,102)",
-  fontSize: "10px",
-  fontWeight: "600",
-  padding: "0px 30px 0px 0px",
-  textAlign: "right" as const,
-};
+  margin: '0',
+  color: 'rgb(102,102,102)',
+  fontSize: '10px',
+  fontWeight: '600',
+  padding: '0px 30px 0px 0px',
+  textAlign: 'right' as const,
+}
 
 const productPrice = {
-  fontSize: "12px",
-  fontWeight: "600",
-  margin: "0",
-};
+  fontSize: '12px',
+  fontWeight: '600',
+  margin: '0',
+}
 
 const productPriceLarge = {
-  margin: "0px 20px 0px 0px",
-  fontSize: "16px",
-  fontWeight: "600",
-  whiteSpace: "nowrap" as const,
-  textAlign: "right" as const,
-};
+  margin: '0px 20px 0px 0px',
+  fontSize: '16px',
+  fontWeight: '600',
+  whiteSpace: 'nowrap' as const,
+  textAlign: 'right' as const,
+}
 
 const productPriceWrapper = {
-  display: "table-cell",
-  padding: "0px 20px 0px 0px",
-  width: "100px",
-  verticalAlign: "top",
-};
+  display: 'table-cell',
+  padding: '0px 20px 0px 0px',
+  width: '100px',
+  verticalAlign: 'top',
+}
 
-const productPriceLine = { margin: "30px 0 0 0" };
+const productPriceLine = { margin: '30px 0 0 0' }
 
 const productPriceVerticalLine = {
-  height: "48px",
-  borderLeft: "1px solid",
-  borderColor: "rgb(238,238,238)",
-};
+  height: '48px',
+  borderLeft: '1px solid',
+  borderColor: 'rgb(238,238,238)',
+}
 
 const productPriceLargeWrapper = {
-  display: "table-cell",
-  width: "90px",
-};
+  display: 'table-cell',
+  width: '90px',
+}
 
-const productPriceLineBottom = { margin: "0 0 75px 0" };
+const productPriceLineBottom = { margin: '0 0 75px 0' }
 
 const footerLinksWrapper = {
-  margin: "8px 0 0 0",
-  textAlign: "center" as const,
-  fontSize: "12px",
-  color: "rgb(102,102,102)",
-};
+  margin: '8px 0 0 0',
+  textAlign: 'center' as const,
+  fontSize: '12px',
+  color: 'rgb(102,102,102)',
+}
 
 const footerCopyright = {
-  margin: "25px 0 0 0",
-  textAlign: "center" as const,
-  fontSize: "12px",
-  color: "rgb(102,102,102)",
-};
+  margin: '25px 0 0 0',
+  textAlign: 'center' as const,
+  fontSize: '12px',
+  color: 'rgb(102,102,102)',
+}
